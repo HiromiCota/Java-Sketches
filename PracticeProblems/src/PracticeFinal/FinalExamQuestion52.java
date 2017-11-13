ec027f6cc6f453e9fc864641b35a710e930f218e
HiromiCota <HiromiCota@users.noreply.github.com>
2017-11-13 10:39:09 -0800

    Created project for practice problems
    Final exam practice problems for Java 1.

---
+++ PracticeProblems/src/PracticeFinal/FinalExamQuestion52.java
@@ -0,0 +1,28 @@
+package PracticeFinal;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class FinalExamQuestion52 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter the number of random integers desired: ");
+        int numInt = keyboard.nextInt();
+        int randInts[] = new int[numInt];
+        Random random = new Random();
+        for (int i = 0; i < numInt; i++) {
+            randInts[i] = random.nextInt(28) - 10;
+            System.out.print("" + randInts[i] + "\t");
+            if (i % 10 == 9) {
+                System.out.print("\n");
+            }
+        }
+
+    }
+
+}
---
+++ PracticeProblems/src/PracticeFinal/FinalExamQuestion54.java
@@ -0,0 +1,42 @@
+package PracticeFinal;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class FinalExamQuestion54 {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter the number of random integers in the range [-21, 69] you want: ");
+        int randomInts = keyboard.nextInt();
+        Random rand = new Random();
+        int[] randNums = new int[randomInts];
+        for (int i = 0; i < randomInts; i++) {
+            randNums[i] = rand.nextInt(91) - 21;
+            if (i % 10 != 9) {
+                System.out.print(randNums[i] + "\t");
+            } else {
+                System.out.print(randNums[i] + "\n");
+            }
+        }
+        System.out.print("Enter number to search array for: ");
+        int key = keyboard.nextInt();
+        int indexFound = linearSearch(key, randNums);
+        if (indexFound == -1) {
+            System.out.println("Key not found");
+        } else {
+            System.out.println("Key found at position " + indexFound);
+        }
+    }
+
+    private static int linearSearch(int key, int[] randNums) {
+        for (int i = 0; i < randNums.length; i++) {
+            if (randNums[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}

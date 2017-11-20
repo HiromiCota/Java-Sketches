dd02edb41a60c56d4714f88266b9bd10cb0e4a34
Hiromi Cota <hiromi.cota@gmail.com>
2017-11-20 11:39:00 -0800

    Fixing some headers

--- Project3/src/Movie/MovieGUI.java
+++ Project3/src/Movie/MovieGUI.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Movie;
 
 /**
  *
- * @author 980453413
+ * @author Hiromi Cota
  */
 public class MovieGUI extends javax.swing.JFrame {
 
